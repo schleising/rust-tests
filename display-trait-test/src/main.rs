65b5c457c0be4c6c0eb1f6e15a83dc5f13d22acb
schleising <stephen@schleising.net>
2023-12-03 17:24:37 +0000

    Create a trait that works on all types that implement Display

---
+++ display-trait-test/src/main.rs
@@ -0,0 +1,28 @@
+use std::fmt::Display;
+
+trait Exclaim where Self: Display {
+    fn exclaim (&self) -> String {
+        format!("{}!", self)
+    }
+}
+
+impl<T: Display> Exclaim for T {}
+
+fn main() {
+    println!("{}", "&str".exclaim());
+    // Create another type that implements Display
+    let s: String = String::from("String");
+    println!("{}", s.exclaim());
+
+    // Create another type that implements Display
+    let s: i32 = 5;
+    println!("{}", s.exclaim());
+
+    // Create another type that implements Display
+    let s: f64 = 5.5;
+    println!("{}", s.exclaim());
+
+    // Create a type that does not implement Display
+    // let s: Vec<i32> = vec![1, 2, 3];
+    // println!("{}", s.exclaim()); // This will not compile
+}
