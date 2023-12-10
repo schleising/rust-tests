c5bfda5be2e0dcc85757d5d99f0880933a068adc
schleising <stephen@schleising.net>
2023-12-10 18:53:55 +0000

    Test the notify crate

---
+++ notify-test/src/main.rs
@@ -0,0 +1,26 @@
+use std::path::Path;
+
+use notify::{Watcher, RecursiveMode, Result};
+
+fn main() -> Result<()> {
+    // Automatically select the best implementation for your platform.
+    let mut watcher = notify::recommended_watcher(|res| {
+        match res {
+           Ok(event) => println!("event: {:?}", event),
+           Err(e) => println!("watch error: {:?}", e),
+        }
+    })?;
+
+    println!("watcher: {:?}", watcher);
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(Path::new("/Users/steve/Downloads"), RecursiveMode::Recursive)?;
+
+    // This is a simple loop, but you may want to use more complex logic here,
+    // for example to handle I/O.
+    loop {
+    }
+
+    // Ok(())
+}
