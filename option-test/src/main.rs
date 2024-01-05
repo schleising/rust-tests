acf4a3019990ba40283590a6af9aff3a89bed574
schleising <stephen@schleising.net>
2024-01-05 12:33:52 +0000

    Refactor take_test and add replace_test functions

--- option-test/src/main.rs
+++ option-test/src/main.rs
@@ -3,20 +3,42 @@ fn take_test() {
     let mut none_i32: Option<i32> = None;
 
     println!("Before take()");
-    println!("some_i32: {:?}", some_i32);
-    println!("none_i32: {:?}", none_i32);
+    dbg!(some_i32);
+    dbg!(none_i32);
 
     let the_i32 = some_i32.take();
     println!("After take() on some_i32");
-    println!("some_i32: {:?}", some_i32);
-    println!("the_i32: {:?}", the_i32);
+    dbg!(some_i32);
+    dbg!(the_i32);
 
     let the_i32 = none_i32.take();
     println!("After take() on none_i32");
-    println!("none_i32: {:?}", none_i32);
-    println!("the_i32: {:?}", the_i32);
+    dbg!(none_i32);
+    dbg!(the_i32);
+    println!();
+}
+
+fn replace_test() {
+    let mut some_i32: Option<i32> = Some(5);
+    let mut none_i32: Option<i32> = None;
+
+    println!("Before replace()");
+    dbg!(some_i32);
+    dbg!(none_i32);
+
+    let the_i32 = some_i32.replace(6);
+    println!("After replace() on some_i32");
+    dbg!(some_i32);
+    dbg!(the_i32);
+
+    let the_i32 = none_i32.replace(7);
+    println!("After replace() on none_i32");
+    dbg!(none_i32);
+    dbg!(the_i32);
+    println!();
 }
 
 fn main() {
     take_test();
+    replace_test();
 }
