b64e919e582ff15aa530eb0bb669ad6cbf2b068f
schleising <stephen@schleising.net>
2024-01-07 21:42:00 +0000

    Add struct Foo and demonstrate lifetime issues in main.rs

--- lifetime-test/src/main.rs
+++ lifetime-test/src/main.rs
@@ -1,3 +1,7 @@
+struct Foo<'a> {
+    x: &'a i32,
+}
+
 fn return_largest<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
     if x > y {
         x
@@ -16,5 +20,19 @@ fn main() {
         z = return_largest(&x, &y);
         println!("The largest number is {}", z);
     }
-    
+
+    // println!("The value of z is     {}", z); // error[E0597]: `y` does not live long enough
+
+    let mut foo: Foo = Foo { x: &x };
+    println!("The value of foo.x is {}", foo.x);
+
+    {
+        let y = 5;
+        foo.x = &y;
+
+        println!("The value of foo.x is {}", foo.x);
+    }
+
+    // println!("The value of foo.x is {}", foo.x); // error[E0597]: `y` does not live long enough
+
 }
