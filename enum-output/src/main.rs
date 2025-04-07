41c360a8e556da0a8ae6130461bef03dcfadd34e
schleising <stephen@schleising.net>
2025-04-07 14:14:25 +0100

    Add initial Cargo.toml and main.rs files for enum-output project

---
+++ enum-output/src/main.rs
@@ -0,0 +1,32 @@
+// struct Message1 {
+//     id: u32,
+//     text: String,
+// }
+
+// struct Message2 {
+//     id: u32,
+//     value: f64,
+// }
+
+// enum TestEnum {
+//     A(Message1),
+//     B(Message2),
+// }
+
+fn main() {
+    // let message1 = Message1 {
+    //     id: 1,
+    //     text: "Hello, World!".to_string(),
+    // };
+
+    // let test_enum1 = TestEnum::A(message1);
+
+    // let output = match test_enum1 {
+    //     TestEnum::A(message) => {
+    //         message
+    //     }
+    //     // TestEnum::B(message) => {
+    //     //     message
+    //     // }
+    // };
+}
