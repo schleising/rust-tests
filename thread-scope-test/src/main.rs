72d8f105833a106550ac04a64fcc695aded5ad9e
schleising <stephen@schleising.net>
2025-05-21 11:51:28 +0100

    Add initial implementation of Cargo.toml and main.rs for thread-scope-test project

---
+++ thread-scope-test/src/main.rs
@@ -0,0 +1,29 @@
+use std::{thread, time};
+
+fn main() {
+    let mut vector = vec![1, 2, 3, 4, 5];
+    println!("Main thread {:?}", vector);
+
+    // Get the current time
+    let start_time = time::Instant::now();
+
+    thread::scope(|scope| {
+        scope.spawn(|| {
+            vector.push(6);
+            println!("Thread {:?}", vector);
+            thread::sleep(time::Duration::from_secs(2));
+        });
+
+        scope.spawn(|| {
+            thread::sleep(time::Duration::from_secs(4));
+        });
+    });
+
+    // Calculate the elapsed time
+    let elapsed_time = start_time.elapsed();
+    println!("Elapsed time: {:.0?}", elapsed_time);
+
+    // Vector is still accessible here
+    vector.push(7);
+    println!("Main thread after scope {:?}", vector);
+}
